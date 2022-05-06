eb00cb5308450cf54cfa3a3c36bd2ad9f56fd161
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-05-06 14:14:01 +0200

    add postItems; props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/Counter';
+import PostItems from './components/PostItems';
+import './styles/App.css'
 
 function App() {
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState('text intro input')
   
   return (
     <div className="App">
@@ -11,6 +13,7 @@ function App() {
       <h2>{value}</h2>
       <input type="text" value={value} onChange={event => setValue(event.target.value)}/>
       <ClassCounter/>
+      <PostItems post={{id:1, title: 'JavaScript', body: 'Description'}} />
       
     </div>
   );
---
+++ src/components/PostItems.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const PostItems = (props) => {
+  return(
+    <div className='post'>
+        <div className='post_content'>
+          <strong>{props.post.id}.{props.post.title}</strong>
+          <div>
+            {props.post.body}
+          </div>
+        </div>
+        <div className='post_btns'>
+          <button>Supprimer</button>
+        </div>
+    </div>
+    
+  )
+}
+export default PostItems;
